60ab4c472d9dac5c06f1796759f5457e9718a0b0
santosh <sbohara579@gmail.com>
2024-04-21 16:57:08 +0545

    footer

--- user/header-footer/footer.php
+++ user/header-footer/footer.php
@@ -1,9 +1,37 @@
 <div class="footer">
+    <div class="center-align">
+        <div class="row">
 
+            <ul>
+                <li>Contact Us</li>
+                <li>Privacy Policy</li>
+                <li>Terms of Services</li>
+            </ul>
+            <div class="copyright">
+                @copyright by OtakuOasis
+            </div>
+        </div>
+    </div>
 </div>
 <style>
+    .center-align ul li {
+        display: inline-block;
+        padding: 0px 9px;
+        margin: 9px 0;
+        border-right: 1px solid #fff;
+    }
+
+    .center-align ul{
+        color: #abb8c3;
+    }
+
+    .copyright{
+        color: #9d9d9d;
+    }
+
     .footer {
-        background: ;
+        background: #0f1320;
+        height: 300px;
     }
 </style>
 </body>
